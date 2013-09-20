05951ed2c92c45bd4c89339bb97ea1a38680b1a9
Jeff Chien <jeff85@gmail.com>
2013-09-20 02:31:01 -0500

    add logging to hardcoded path, add simple error page, add timing info for requests

--- app/twitch_browser.py
+++ app/twitch_browser.py
@@ -1,4 +1,8 @@
 import json
+import logging
+import logging.handlers
+import os
+import time
 import urllib
 import urllib2
 
@@ -8,12 +12,35 @@ import twitch_stream
 
 app = Flask(__name__)
 
+def start_server():
+    init_logging()
+    app.logger.info('starting server')
+    app.run('0.0.0.0', debug=True, use_reloader=False, use_evalex=False, threaded=True)
+
+def init_logging():
+    app.logger.setLevel(logging.DEBUG)
+    log_path = os.path.join('c:/', 'cygwin', 'var', 'log', 'twitch-browser', 'twitch-browser.log')
+    handler = logging.handlers.TimedRotatingFileHandler(
+        log_path,
+        when='midnight',
+        interval=1,
+        backupCount=28)
+    handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)-8s [%(module)s] %(msg)s'))
+    app.logger.addHandler(handler)
 
 @app.route('/')
 def index():
-    streams = getMultiGameStreams(['dota 2', 'starcraft 2', 'dark souls'])
+    games = ['dota 2', 'starcraft 2', 'dark souls']
+    start = time.time()
+    streams = getMultiGameStreams(games)
+    tot_time = time.time() - start
+    app.logger.info('retrieved streams for %d games in %f secs' % (len(games), tot_time))
     return render_template("index.html", streams=streams)
 
+@app.errorhandler(500)
+def handleError(exc):
+    app.logger.exception(exc)
+    return render_template("error.html")
 
 def getMultiGameStreams(game_names):
     streams = []
@@ -24,10 +51,12 @@ def getMultiGameStreams(game_names):
 
 
 def fetchStreams(game_name):
+    start = time.time()
     host = "https://api.twitch.tv"
     path = "/kraken/search/streams"
     params = urllib.urlencode({
-        'q': game_name
+        'q': game_name,
+        'limit': 20,
     })
     url = "%s%s?%s" % (host, path, params)
     headers = {
@@ -36,7 +65,10 @@ def fetchStreams(game_name):
     }
     req = urllib2.Request(url, headers=headers)
     resp = urllib2.urlopen(req)
-    return resp.read()
+    content = resp.read()
+    tot_time = time.time() - start
+    app.logger.info('took %f secs to retrieve %s @ %s' % (tot_time, game_name, url))
+    return content
 
 
 def parseStreams(raw_content):
@@ -47,4 +79,5 @@ def parseStreams(raw_content):
     return streams
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    #print json.dumps(json.loads(fetchStreams('starcraft 2')), indent=4)
+    start_server()
