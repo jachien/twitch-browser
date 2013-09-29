c27e122d681683af63e14f2e371f1fb3f29000f1
Jeff Chien <jeff85@gmail.com>
2013-09-29 03:29:21 -0500

    use single OR query for searching instead of separate queries for each game

--- app/twitch_browser/views.py
+++ app/twitch_browser/views.py
@@ -11,11 +11,8 @@ from twitch_browser import twitch_stream
 
 @app.route('/')
 def index():
-    games = ['dota 2', 'starcraft 2', 'dark souls']
-    start = time.time()
-    streams = getMultiGameStreams(games)
-    tot_time = time.time() - start
-    app.logger.info('retrieved streams for %d games in %f secs' % (len(games), tot_time))
+    games = ['Dota 2', 'StarCraft II: Heart of the Swarm', 'Dark Souls']
+    streams = getGameStreams(games)
     return render_template("index.html", streams=streams)
 
 @app.errorhandler(500)
@@ -23,21 +20,31 @@ def handleError(exc):
     app.logger.exception(exc)
     return render_template("error.html")
 
-def getMultiGameStreams(game_names):
-    streams = []
+def getGameStreams(game_names):
+    query = buildQuery(game_names)
+    raw_content = fetchStreams(query)
+    return parseStreams(raw_content)
+
+
+def buildQuery(game_names):
+    q = ''
+    first = True
     for game_name in game_names:
-        raw_content = fetchStreams(game_name)
-        streams += parseStreams(raw_content)
-    return sorted(streams, key=lambda stream: (stream.getViewerCount(), stream.getUserDisplayName()), reverse=True)
+        if first:
+            first = False
+        else:
+            q += ' OR '
+        q += '"%s"' % game_name
+    return q
 
 
-def fetchStreams(game_name):
+def fetchStreams(query):
     start = time.time()
     host = "https://api.twitch.tv"
     path = "/kraken/search/streams"
     params = urllib.urlencode({
-        'q': game_name,
-        'limit': 20,
+        'q': query,
+        'limit': 50,
         })
     url = "%s%s?%s" % (host, path, params)
     headers = {
@@ -48,7 +55,7 @@ def fetchStreams(game_name):
     resp = urllib2.urlopen(req)
     content = resp.read()
     tot_time = time.time() - start
-    app.logger.info('took %f secs to retrieve %s @ %s' % (tot_time, game_name, url))
+    app.logger.info('took %f secs to retrieve %s @ %s' % (tot_time, query, url))
     return content
 
 
